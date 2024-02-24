1705cd389f1d478443e325934b59fc14e551af01
Denis Gukov <denis.guk@konomic.com>
2024-02-24 23:29:33 +0300

    steam: fixed user request, added ban info

--- controllers/steamController.py
+++ controllers/steamController.py
@@ -7,22 +7,35 @@ import json
 class Steam():
     steam_url = config('STEAM_URL')
     steam_key = config('STEAM_KEY')
-    def get_steam_html(self):
-        path = request.args.get('path')
-        print(path)
-        if not path:
-            return Api('Steam link invalid').bad_request()
-        
+    def get_banned_user_data(self, path):
         html = urllib.request.urlopen(path).read().decode("utf-8")
         steamId = html.split('"steamid":"')[1].split('",')[0]
-        response = { "steamId": steamId }
-        return Api("User has found", response).response()
+        result = {
+            "steamId": steamId
+        }
+        if 'class="profile_ban_info"' in html:
+            result['banned'] = True
+        else:
+            result['banned'] = False
+        return result
+
+
+    def get_steam_html(self):
+        path = request.args.get('path')
+        if not path:
+            return Api('Steam link invalid').bad_request()
+        user = self.get_banned_user_data(path)
+        return Api("User has found", user).response()
     
     def get_banned_users_data(self):
         list = request.json['banned_list']
         data = urllib.request.urlopen(f"{self.steam_url}?key={self.steam_key}&steamids={','.join(list)}").read().decode("utf-8")
         if not data:
             return Api('Something went wrong on server side').internal()
-        print(json.loads(data)['response'])
-        return Api(data=json.loads(data)['response']).response()
+        result = []
+        for player in json.loads(data)['response']['players']:
+            new_item = player
+            new_item['banned'] = self.get_banned_user_data(path=player["profileurl"])["banned"]
+            result.append(new_item)
+        return Api(data={"players": result}).response()
 
